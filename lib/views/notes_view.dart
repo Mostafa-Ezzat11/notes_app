e1207150d4ae3ff66dc9dcad867c201be7ffb777
Mostafa Ezzat <mzt292237@gmail.com>
2026-07-24 01:21:50 +0300

    add flouting action bottom and make Note List View

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
-import 'package:notes_app/widgets/custom_note_item.dart';
+import 'package:notes_app/widgets/notes_listview.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteBottomsheet();
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      body: const Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: [
-            SizedBox(height: 70),
+            SizedBox(height: 60),
             CustomAppbar(),
-            NoteItem(),
+            Expanded(child: NotesListView()),
           ],
         ),
       ),
